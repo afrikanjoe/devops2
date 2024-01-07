5888f895fb8ee483dda69b0fa483d1bda4b375f3
Joseph Mugisha <josephmugisha@josephs-mbp.lan>
2024-01-06 23:48:16 -0500

    init of the repo

---
+++ calculator.py
@@ -0,0 +1,14 @@
+"""
+calculator library containing basic math operations.
+"""
+
+def add(first_term,second_term):
+    return first_term + second_term
+
+def multiply(first_term,second_term):
+    return first_term * second_term
+
+def subtract(first_term,second_term):
+    return first_term - second_term
+
+    
